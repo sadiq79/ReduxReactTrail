084fa8b96bf1dbecf2d836f7836e3224a010a381
sadiq79 <sadiq.md.ali@gmail.com>
2019-03-28 15:53:21 +0530

    trail

---
+++ src/components/header.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import {connect} from 'react-redux'
+import webConfig from './../../../webconfig'
+import classNames from 'classNames'
+
+export default class header extends Component {
+    constructor(props) {
+        super();
+    
+    }
+  render() {
+    return (
+        <header onScroll={this.listenScrollEvent} className={classNames({'scrollActive': this.state.vPos > 0, 'mobileNavActive': this.state.mobileToggle})}>
+            <div className='logo_wrap'>
+                <Link to='/'>
+                    <img src={`${webConfig.siteURl}/assets/graphics/logo.png`} alt='Logo' className='dark'/>
+                    <img src={`${webConfig.siteURl}/assets/graphics/logo_white.png`} alt='Logo' className='white'/>
+                </Link>
+            </div>
+            <div className='mobile'>
+                <span className={classNames({'icon':true, 'mobileNavActive':this.state.mobileToggle})} onClick={this.toggleMobileNav.bind(this)}>
+                    <span></span>
+                </span>
+            </div>
+            <div className={classNames({'nav_wrap':'true', 'mobile_active':this.state.mobileToggle})}>
+                <nav>
+                    <ul>
+                        <li className='first'>
+                            <NavLink activeClassName='active' to='/blog'>Blog</NavLink>
+                        </li>
+                        <li className='first'>
+                            <NavLink activeClassName='active' to='/services'>Services</NavLink>
+                        </li>
+                        <li className='first'>
+                            <NavLink activeClassName='active' to='/about'>About</NavLink>
+                        </li>
+                        <li className='first'>
+                            <NavLink activeClassName='active' to='/contact'>Contact</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+    
+  }
+}
