1ba0ae30a8bba4c155d7c08f6de43deb6a487d30
sadiq79 <sadiq.md.ali@gmail.com>
2019-03-28 16:32:22 +0530

    imported component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Footer from './components/footer';
+import Header from './components/header';
+import {renderRoutes} from 'react-router-config';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Header />
+      {renderRoutes(this.props.route.routes)}
+      <Footer />
        
       </div>
     );
